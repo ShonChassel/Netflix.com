85c957be3692b0448aa36345e3a2811e0ece266d
Shon Chassel <114100392+ShonChassel@users.noreply.github.com>
2023-10-23 12:11:35 +0300

    Add files via upload

---
+++ components/components/Header.tsx
@@ -0,0 +1,66 @@
+import { BellIcon, SearchIcon } from "@heroicons/react/solid";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
+import BasicMenu from "./BasicMenu";
+
+function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+    const { logout } = useAuth();
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <header className={`${isScrolled && "bg-[#141414]"}`}>
+            <div className="flex items-center space-x-2 md:space-x-10">
+                <img
+                    src="https://res.cloudinary.com/dirvusyaz/image/upload/v1697999549/pngwing.com_ljpjjg.png"
+                    width={100}
+                    height={100}
+                    className="cursor-pointer object-contain"
+                />
+
+                <BasicMenu />
+
+                <ul className="hidden space-x-4 md:flex">
+                    <li className="headerLink">Home</li>
+                    <li className="headerLink">TV Shows</li>
+                    <li className="headerLink">Movies</li>
+                    <li className="headerLink">New & Popular</li>
+                    <li className="headerLink">My List</li>
+                </ul>
+            </div>
+
+            <div className="flex items-center space-x-4 text-sm font-light">
+                <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
+                <p className="hidden lg:inline">Kids</p>
+                <BellIcon className="h-6 w-6" />
+                <Link href="/account">
+                    <img
+                        src="https://res.cloudinary.com/dirvusyaz/image/upload/v1697999821/Netflix-avatar_ee5ewj.png"
+                        alt=""
+                        width={30}
+                        height={30}
+                        className="cursor-pointer rounded"
+                    />
+                </Link>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
---
+++ components/components/Thumbnail.tsx
@@ -0,0 +1,70 @@
+import { DocumentData } from "firebase/firestore";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import { useRecoilState } from "recoil";
+import { modalState, movieState } from "../atoms/modalAtom";
+import { Movie } from "../typings";
+import TopTenImg from "../utils/topTen";
+
+interface Props {
+    movie: Movie | DocumentData
+    special?: boolean;
+    TopTen?: string[];
+}
+
+function Thumbnail({ movie, special, TopTen }: Props) {
+    const [showModal, setShowModal] = useRecoilState(modalState)
+    const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+
+    let isTop: any = null;
+
+    const handleTopTen = () => {
+        isTop = TopTenImg;
+    };
+
+    useState(() => {
+        handleTopTen();
+    });
+
+    return (
+        <div>
+            {special ? (
+                <div className="topTen" onClick={() => {
+                    setCurrentMovie(movie)
+                    setShowModal(true)
+                  }}>
+                    
+                        
+                    
+                    <Image
+                        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                        className="topTenImg rounded-sm object-cover md:rounded max-h-[180px] w-auto "
+                        layout="fill"
+                        alt=""
+                    />
+                </div>
+            ) : (
+                <div
+                    className="relative h-28 min-w-[180px] cursor-pointer transition duration-200
+                     ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+                     onClick={() => {
+                        setCurrentMovie(movie)
+                        setShowModal(true)
+                      }}>
+                    <Image
+                        src={`https://image.tmdb.org/t/p/w500${
+                            special
+                                ? movie.poster_path
+                                : movie.backdrop_path || movie.poster_path
+                        }`}
+                        className="rounded-sm object-cover md:rounded"
+                        layout="fill"
+                        alt=""
+                    />
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Thumbnail;
