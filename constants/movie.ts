216d4e36b40bfdd2a2e2c73fe9295e09262a5992
Shon Chassel <114100392+ShonChassel@users.noreply.github.com>
2023-10-23 12:10:17 +0300

    Update movie.ts

--- constants/movie.ts
+++ constants/movie.ts
@@ -1,1 +1,15 @@
-export const baseUrl = 'https://image.tmdb.org/t/p/original/'
+export const baseUrl = 'https://image.tmdb.org/t/p/original
+
+export const TopTen = [
+    'https://res.cloudinary.com/dirvusyaz/image/upload/v1674832752/Netflix-0_gzjfmz.svg',
+    'https://res.cloudinary.com/dirvusyaz/image/upload/v1674832752/Netflix-1_elz36g.svg',
+    'https://res.cloudinary.com/dirvusyaz/image/upload/v1674832752/Netflix-2_hi8ufk.svg',
+    'https://res.cloudinary.com/dirvusyaz/image/upload/v1674832752/Netflix-3_c4typd.svg',
+    'https://res.cloudinary.com/dirvusyaz/image/upload/v1674835477/Netflix-10_kggk5g.svg',
+    'https://res.cloudinary.com/dirvusyaz/image/upload/v1674834941/Netflix-4_qrqsmc.svg',
+    'https://res.cloudinary.com/dirvusyaz/image/upload/v1674832752/Netflix-5_wzk7yv.svg',
+    'https://res.cloudinary.com/dirvusyaz/image/upload/v1674832753/Netflix-6_nh0gyt.svg',
+    'https://res.cloudinary.com/dirvusyaz/image/upload/v1674832753/Netflix-7_vcvwki.svg',
+    'https://res.cloudinary.com/dirvusyaz/image/upload/v1698049726/Netflix-7_vcvwki_1_mi1z2d.svg',
+
+]
